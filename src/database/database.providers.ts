1a028c9322d1bb06989bca71ba72194528d36a0c
pietro-rutzen <rutzenpietro@gmail.com>
2024-07-18 15:54:59 +0300

    fix: adjust dbname

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,6 +6,8 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     inject: [ConfigService],
     useFactory: (configService: ConfigService): Promise<typeof mongoose> =>
-      mongoose.connect(configService.get<string>('MONGODB_CONNECTION_STRING')),
+      mongoose.connect(configService.get<string>('MONGODB_CONNECTION_STRING'), {
+        dbName: configService.get<string>('MONGODB_DATABASE'),
+      }),
   },
 ];
