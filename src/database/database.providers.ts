8601a2d2b4baad5c45f3554b162f08d5add3fb2d
pietro-rutzen <rutzenpietro@gmail.com>
2024-07-18 18:11:53 +0300

    chore: use constant for db connection

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,9 +1,10 @@
 import { ConfigService } from '@nestjs/config';
 import * as mongoose from 'mongoose';
+import { DATABASE_CONNECTION } from 'src/constants';
 
 export const databaseProviders = [
   {
-    provide: 'DATABASE_CONNECTION',
+    provide: DATABASE_CONNECTION,
     inject: [ConfigService],
     useFactory: (configService: ConfigService): Promise<typeof mongoose> =>
       mongoose.connect(configService.get<string>('MONGODB_CONNECTION_STRING'), {
