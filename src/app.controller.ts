6f74cdf4e5efc7df78a9f00049ade879917623a4
pietro-rutzen <rutzenpietro@gmail.com>
2024-07-20 20:43:51 +0300

    chore: redis test endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import Redis from 'ioredis';
+import { RedisService } from 'nestjs-redis';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly redisClient: Redis;
+
+  constructor(
+    private readonly redisService: RedisService,
+    private readonly appService: AppService,
+  ) {
+    this.redisClient = this.redisService.getClient();
+  }
+
+  @Get('redis/set/:key/:value')
+  async setKey(
+    @Param('key') key: string,
+    @Param('value') value: string,
+  ): Promise<string> {
+    await this.redisClient.set(key, value);
+    return `Redis key ${key} was set with this value: ${value}`;
+  }
+
+  @Get('redis/get/:key')
+  async getKey(@Param('key') key: string): Promise<string> {
+    const value = await this.redisClient.get(key);
+    return value
+      ? `Value for Redis key ${key} is ${value}`
+      : `Key ${key} not found`;
+  }
 
   @Get()
   getHello(): string {
