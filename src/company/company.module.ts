1a415605bd49fc2f2e5cf8b2ae894af9f54aeed4
pietro-rutzen <rutzenpietro@gmail.com>
2024-07-20 20:59:26 +0300

    chore: cache to company service

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
+import { LocalRedisModule } from 'src/redis/redis.module';
 import { companyProviders } from './company.providers';
 import { CompanyService } from './company.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, LocalRedisModule],
   providers: [CompanyService, ...companyProviders],
   exports: [CompanyService],
 })
--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,16 +1,29 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { Redis } from 'ioredis';
 import { Model, Types } from 'mongoose';
-import { COMPANY_MODEL } from 'src/constants';
+import { RedisService } from 'nestjs-redis';
+import { COMPANY_MODEL, ONE_HOUR_IN_SECONDS } from 'src/constants';
 import { Company } from './interfaces/company.interface';
 
 @Injectable()
 export class CompanyService {
+  private readonly redisClient: Redis;
+
   constructor(
     @Inject(COMPANY_MODEL)
     private companyModel: Model<Company>,
-  ) {}
+    private readonly redisService: RedisService,
+  ) {
+    this.redisClient = this.redisService.getClient();
+  }
 
   async getAllChildCompanyIds(parentCompanyId: string): Promise<string[]> {
+    const cacheKey = `company:children:${parentCompanyId}`;
+    const cachedData = await this.redisClient.get(cacheKey);
+    if (cachedData) {
+      return JSON.parse(cachedData);
+    }
+
     const result = await this.companyModel
       .aggregate([
         {
@@ -40,7 +53,16 @@ export class CompanyService {
       .exec();
 
     if (result.length > 0) {
-      return result[0].allChildren.map((id: string) => new Types.ObjectId(id));
+      const childCompanyIds = result[0].allChildren.map(
+        (id: string) => new Types.ObjectId(id),
+      );
+      await this.redisClient.set(
+        cacheKey,
+        JSON.stringify(childCompanyIds),
+        'EX',
+        ONE_HOUR_IN_SECONDS,
+      );
+      return childCompanyIds;
     } else {
       return [];
     }
