29ad20e36b5b6baca55fa8a07ea05e177ffadd72
pietro-rutzen <rutzenpietro@gmail.com>
2024-07-22 12:01:10 +0300

    chore: remove redis test endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import Redis from 'ioredis';
 import { RedisService } from 'nestjs-redis';
 import { AppService } from './app.service';
@@ -12,23 +12,6 @@ export class AppController {
     private readonly appService: AppService,
   ) {
     this.redisClient = this.redisService.getClient();
-  }
-
-  @Get('redis/set/:key/:value')
-  async setKey(
-    @Param('key') key: string,
-    @Param('value') value: string,
-  ): Promise<string> {
-    await this.redisClient.set(key, value);
-    return `Redis key ${key} was set with this value: ${value}`;
-  }
-
-  @Get('redis/get/:key')
-  async getKey(@Param('key') key: string): Promise<string> {
-    const value = await this.redisClient.get(key);
-    return value
-      ? `Value for Redis key ${key} is ${value}`
-      : `Key ${key} not found`;
   }
 
   @Get()
